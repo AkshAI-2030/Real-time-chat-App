8b1d0f4746eb34591a5d0f8f1b42d2ca15fc22f5
AkshAI-2030 <akshayarelli2030@gmail.com>
2025-02-10 10:58:03 +0530

    polling

--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -6,10 +6,13 @@ import "./chat.css";
 
 const ENDPOINT =
   "https://real-time-chat-app-backend-1prx4u75t-akshay-arellis-projects.vercel.app"; // Define endpoint
-const socket = io(ENDPOINT, {
-  transports: ["websocket", "polling"], // Allow both transport types
-  withCredentials: true, // Ensure authentication cookies are sent
-});
+const socket = io(
+  "https://real-time-chat-app-backend-1prx4u75t-akshay-arellis-projects.vercel.app",
+  {
+    transports: ["websocket", "polling"], // Allow both transport types
+    withCredentials: true, // Ensure authentication cookies are sent
+  }
+);
 
 export const Chat = ({ user }) => {
   const [users, setUsers] = useState([]);
